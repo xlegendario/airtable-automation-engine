cdf4e206647b652e5083dddfb316775531182549
xlegendario <dario.b1996@gmail.com>
2026-05-08 11:02:42 +0200

    Update automationRegistry.js

--- src/automationRegistry.js
+++ src/automationRegistry.js
@@ -1,5 +1,7 @@
 import { unfulfilledOrderCreated } from "./automations/unfulfilledOrderCreated.js";
+import { unfulfilledOrderAllocated } from "./automations/unfulfilledOrderAllocated.js";
 
 export const automations = [
   unfulfilledOrderCreated,
+  unfulfilledOrderAllocated,
 ];
