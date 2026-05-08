0d52b8cef2c9b5838f795ad865221b82b35dd4de
xlegendario <dario.b1996@gmail.com>
2026-05-08 11:25:31 +0200

    Update automationRegistry.js

--- src/automationRegistry.js
+++ src/automationRegistry.js
@@ -1,9 +1,11 @@
 import { unfulfilledOrderCreated } from "./automations/unfulfilledOrderCreated.js";
 import { unfulfilledOrderAllocated } from "./automations/unfulfilledOrderAllocated.js";
 import { autoAllocateBestUnit } from "./automations/autoAllocateBestUnit.js";
+import { calculateLinkedUnitPrice } from "./automations/calculateLinkedUnitPrice.js";
 
 export const automations = [
   unfulfilledOrderCreated,
   unfulfilledOrderAllocated,
   autoAllocateBestUnit,
+  calculateLinkedUnitPrice,
 ];
