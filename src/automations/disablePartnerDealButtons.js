9ee73bfc2f932e4eef150ce03921e5d3c751022a
xlegendario <dario.b1996@gmail.com>
2026-05-15 08:23:18 +0200

    Update disablePartnerDealButtons.js

--- src/automations/disablePartnerDealButtons.js
+++ src/automations/disablePartnerDealButtons.js
@@ -27,7 +27,10 @@ export const disablePartnerDealButtons = {
       "Fulfilled",
       "Ready to Ship",
       "Awaiting Label",
+      "Requested Label",
       "Allocated",
+      "Found",
+      "Confirmed",
     ];
 
     return (
